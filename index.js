7f6f2560df06f93cd77fd1afcc11978a15656126
Khess S Dotto <khessdotto@gmail.com>
2023-02-20 18:04:03 -0300

    Done all iterations but the bonuses.

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.post("/create", (req, res) => {
 
 app.put("/edit/:id", (req, res) => {
   const { id } = req.params;
-  console.log(id);
   const index = data.findIndex((cE) => cE.id === id);
   data.splice(index, 1, { ...req.body, id: id });
   const modEntry = data.find((cE) => cE.id === id);
@@ -37,14 +36,31 @@ app.delete("/delete/:id", (req, res) => {
   return res.status(202).json(delEntry);
 });
 
-// ------------
+// ---------------------------------------------------------------------------------------------------------------
 
-// app.get("");
+app.get("/process/:id", (req, res) => {
+  const { id } = req.params;
+  const reqEntry = data.find((cE) => cE.id === id);
+  return res.status(200).json(reqEntry);
+});
 
-// app.put();
+app.put("/addComment/:id", (req, res) => {
+  const { id } = req.params;
+  const index = data.findIndex((cE) => cE.id === id);
+  let reqEntry = data.find((cE) => cE.id === id);
+  const entryComments = reqEntry.comments;
+  const { newComment } = req.body;
+  entryComments.push(newComment);
+  reqEntry = data.find((cE) => cE.id === id);
+  return res.status(202).json(reqEntry);
+});
 
-// app.get();
+app.get("/status/open", (req, res) => {
+  const response = data.filter((cE) => cE.status === "Em andamento");
+  return res.status(200).json(response);
+});
 
-// app.get();
-
-// app.get();
+app.get("/status/close", (req, res) => {
+  const response = data.filter((cE) => cE.status === "Finalizado");
+  return res.status(200).json(response);
+});
